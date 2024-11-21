9be23330ce2e36ffc2c4ecfc89188f68b4614fb9
adarsh <111566026+adarshpatel111@users.noreply.github.com>
2024-11-21 16:27:16 +0530

    Create Home.jsx

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Stack } from "@mui/material";
+import QueAns from "../Components/QueAns/QueAns";
+const Home = () => {
+  return (
+    <Stack sx={{ width: "80%", margin: "auto" }}>
+      <div>Home</div>
+      <QueAns />
+    </Stack>
+  );
+}
+export default Home
