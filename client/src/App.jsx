9b33cc26ce01d07d61085eb164f57c1fbb507782
adarsh <111566026+adarshpatel111@users.noreply.github.com>
2024-11-21 16:26:56 +0530

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Home from "./Pages/Home";
+import Footer from "./Components/Footer/Footer";
+const App = () => {
+  return (
+    <>
+      <Home />
+      {/* <Footer /> */}
+    </>
+  );
+};
+export default App;
