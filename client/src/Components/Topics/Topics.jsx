40558706c66acf56ae7fb1e158591e6be59340bf
adarsh <111566026+adarshpatel111@users.noreply.github.com>
2024-11-21 16:27:06 +0530

    Create Topics.jsx

---
+++ client/src/Components/Topics/Topics.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+
+export default function Topics() {
+    const chips=[
+        {
+            label:"React",
+            color:"primary"
+        },
+        {
+            label:"DSA",
+            color:"secondary"
+        }
+    ]
+  return (
+    <>
+    </>
+  );
+}
