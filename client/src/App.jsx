4c467c65c8dca6a27363e0eacfe6772a33bf1c2d
adarsh <111566026+adarshpatel111@users.noreply.github.com>
2024-11-22 16:47:00 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import "./App.css";
 import Home from "./Pages/Home";
 import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
 const App = () => {
   return (
     <>
+      <Navbar />
       <Home />
       {/* <Footer /> */}
     </>
