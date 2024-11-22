04e16e587de7bd533dab8810750b837f1081aa97
adarsh <111566026+adarshpatel111@users.noreply.github.com>
2024-11-22 16:47:22 +0530

    Update Home.jsx

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,19 @@
 import { Stack } from "@mui/material";
 import QueAns from "../Components/QueAns/QueAns";
+import Banner from "../Components/Banner/Banner";
 const Home = () => {
   return (
-    <Stack sx={{ width: "80%", margin: "auto" }}>
-      <div>Home</div>
-      <QueAns />
+    <Stack
+      sx={{
+        width: "100%",
+        BackgroundImage: "url(./Images/Background.png)",
+      }}
+    >
+      <Banner />
+      <Stack sx={{ width: "80%", margin: "auto" }}>
+        <QueAns />
+      </Stack>
     </Stack>
   );
-}
-export default Home
+};
+export default Home;
